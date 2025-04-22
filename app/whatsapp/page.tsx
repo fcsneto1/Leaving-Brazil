98200c83d792469254c16cffdebd669a3c292b34
Francisco Neto <fc.neto@yahoo.com.br>
2025-04-22 23:39:26 +0900

    wpp redirect

---
+++ app/whatsapp/page.tsx
@@ -0,0 +1,6 @@
+// app/whatsapp/page.tsx
+import { redirect } from 'next/navigation';
+
+export default function WhatsAppRedirect() {
+  redirect('https://chat.whatsapp.com/JDCaX3PStFEGSbxtrPMrlN');
+}
